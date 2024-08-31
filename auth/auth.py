70ea57a33d2c30ea619654776260f7627c9511cf
gastyCode <andrejmarkus274@gmail.com>
2024-08-31 18:40:10 +0200

    Auth forms

--- auth/auth.py
+++ auth/auth.py
@@ -1,3 +1,5 @@
+from cgitb import reset
+
 import bcrypt
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
@@ -11,7 +13,21 @@ def login():
 
 @auth.post('/login')
 def login_post():
-    user = User.objects.filter(username=request.form['username'])
+    user = User.objects.filter(username=request.form['username']).first()
+
+    if not user:
+        flash("User isn't registered")
+        return redirect(url_for('auth.login'))
+
+    salt = user.salt
+    hash = bcrypt.hashpw(request.form['password'].encode('utf-8'), salt)
+    result = bcrypt.checkpw(user.password, hash)
+
+    if result:
+        return redirect(url_for('/'))
+
+    flash("Incorrect username or password")
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup')
 def signup():
@@ -22,6 +38,12 @@ def signup_post():
     email = request.form['email']
     username = request.form['username']
     password = request.form['password']
+    confirm_password = request.form['confirm_password']
+
+    if password != confirm_password:
+        flash('Passwords do not match')
+        return redirect(url_for('auth.signup'))
+
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class User(me.Document):
     username = me.StringField(max_length=50, required=True, unique=True)
     email = me.StringField(max_length=50, unique=True)
     salt = me.StringField(required=True)
-    password = me.StringField(max_length=50, required=True)
+    password = me.StringField(max_length=256, required=True)
     role = me.StringField(max_length=50, default='user')
 
 
@@ -18,7 +18,8 @@ class Book(me.Document):
     genre = me.StringField(max_length=50, required=True)
 
 
-class Borrowing(me.EmbeddedDocument):
+class Borrowing(me.Document):
     user_id = me.ReferenceField(User)
+    book_id = me.ReferenceField(Book)
     from_date = me.DateTimeField()
     to_date = me.DateTimeField()
